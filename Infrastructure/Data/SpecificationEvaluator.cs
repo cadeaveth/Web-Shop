ddea3b41f017d6174a6ea6d0b48f4ad0fdbb790d
Stefan <cadeaveth@gmail.com>
2023-10-26 12:58:20 +0200

    generic implementation 42

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -6,10 +6,12 @@ namespace Infrastructure.Data
 {
     public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
     {
-        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec){
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
             var query = inputQuery;
 
-            if (spec.Criteria != null){
+            if (spec.Criteria != null)
+            {
                 query = query.Where(spec.Criteria);
             }
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
