d86b645aa0db6d8827c26dccfb8218d84ffb82b9
Stefan <cadeaveth@gmail.com>
2023-11-11 18:20:14 +0100

    Shop Service add 89

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Product } from '../shared/models/product';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent {
+  products: Product[] = [];
+
+
+  constructor(private shopService: ShopService){}
+
+  ngOnInit(){
+    this.shopService.getProducts().subscribe(response => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+}
